f42f9bb8f9045b9cc495115006d80e3a73d61a1c
Stephen Eilert <spedrosa@gmail.com>
2011-01-14 01:29:44 -0300

    - More brainstorming.

--- real.scm
+++ real.scm
@@ -1,5 +1,19 @@
 (use coops)
 
+(define-class <fragment> ()
+  (name))
+
+(define-method (->sql (stmt <fragment>))
+  (sql-quote name))
+
+(define-class <field> (<fragment>)
+  (name))
+
+(define-class <table> (<fragment>))
+
+(define-class <join> (<fragment>)
+  (tables))
+
 (define-class <query> ()
   (table
    fields
@@ -11,19 +25,72 @@
    order-by
    limit))
 
+(define-class <condition> (<fragment>)
+  (children))
+
+(define-method (has-children? (object  <condition>))
+  (slot-value object 'children))
+
+(define-class <where> (<condition>))
+
 (define (from table #!optional fields)
-  (make <select> 'table table 'fields fields))
+  (let ([select (make <select> 'table (make <table> 'name table) 'fields (add-fields fields))])
+    select))
+
+(define (add-fields fields)
+  (let ([->field (lambda (f)
+                   (make <field> 'name f))])
+    (if fields
+        (if (list? fields)
+            (map ->field fields)
+            (list (->field fields)))
+        (list (->field "*")))))
+
+(define *conditions*
+  '(
+    (= sql-=)
+    (like sql-like)
+    (>= sql->=)
+    (<= sql-<=)
+    (in sql-in)
+    (and sql-and)
+    (or sql-or)))
+
+(define (with-value-if-present object field #!key default procedure)
+  (let ([value (slot-value object field)])
+    (if value
+        (if procedure
+            (procedure value)
+            value)
+        (if default
+            default
+            ""))))
 
 (define-method (->sql (object <select>))
   (string-trim
    (string-append
     (string-intersperse (list " "
                               "select"
-                              (if (slot-value object 'fields)
-                                  (sql-quote (slot-value object 'fields))
-                                  "*")
+                              (sql-enumerate (slot-value object 'fields))
                               "from"
-                              (sql-quote (slot-value object 'table) quote-type: "\""))))))
+                              (->sql (slot-value object 'table)))))))
+
+(define-method (->sql (fragment <fragment>))
+  (sql-quote (slot-value fragment 'name)))
+
+(define-method (->sql (field <field>))
+  (if (equal? (slot-value field 'name) "*")
+      "*"
+      (sql-quote (slot-value field 'name))))
+
+(define-method (->sql (table <table>))
+  (sql-quote (slot-value table 'name) quote-type: "\""))
+
+(define (sql-enumerate lst)
+  (if (list? lst)
+      (string-intersperse (map (lambda (item)
+                                 (sql-enumerate item)) lst) ",")
+      (->sql lst)))
 
 (define (sql-quote something #!key (quote-type "'"))
   (if (list? something)
--- real_tests.scm
+++ real_tests.scm
@@ -9,4 +9,8 @@
                   (->sql (from "teste")))
             (test "Should be able to perform a select, returning the given fields"
                   "select 'field1','field2','field3' from \"teste\""
-                  (->sql (from "teste" '(field1 field2 field3)))))
+                  (->sql (from "teste" '(field1 field2 field3))))
+            (test-group "Conditions"
+                        (test "Should be able to perform a simple where"
+                              "select * from \"teste\" where 1 = 1"
+                              (->sql (where (= 1 1 (from "teste")))))))
